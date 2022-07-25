7a13eb70ed6e46fce395b7ed0bc7f8b5e85a2f2f
Alina <example@gmail.com>
2022-07-25 15:31:38 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+// let number=5;
+// const leftBorderWidth=5;
+
+// number=55;
+// console.log(number);
+
+// const obj={
+//     a:50
+// };   
+// console.log(obj);
+
+// alert('Hello');
+// const result=confirm ("Are you ready?");
+// console.log(result);
+
+const answer=prompt("dff jglfj fjgk?", "");
+
+console.log(typeof (answer));
+
+
+
