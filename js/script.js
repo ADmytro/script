348fd1684e987fd11f0b0104aa3ec7d1f19467d1
Alina <example@gmail.com>
2022-07-26 20:54:10 +0300

    first practice

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 // let number=5;
 // const leftBorderWidth=5;
 
@@ -14,9 +14,30 @@
 // const result=confirm ("Are you ready?");
 // console.log(result);
 
-const answer=prompt("dff jglfj fjgk?", "");
+// const answer=prompt("dff jglfj fjgk?", "");
 
-console.log(typeof (answer));
+// console.log(typeof (answer));
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+
+const personalMovieDB={
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a= prompt("Один из последних просмотренных фильмов?", ""),
+      b= prompt("Насколько оценете его?",""),
+      c= prompt("Один из последних просмотренных фильмов?", ""),
+      d= prompt("Насколько оценете его?","");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
+
+
 
 
 
