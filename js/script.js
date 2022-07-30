fe7fd5d5c1a0a37f899a37ecce031d0a802f3416
Alina <example@gmail.com>
2022-07-30 22:40:56 +0300

    задачки

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 // let number=5;
 // const leftBorderWidth=5;
 
@@ -18,25 +18,61 @@
 
 // console.log(typeof (answer));
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
-const personalMovieDB={
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB={
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a= prompt("Один из последних просмотренных фильмов?", ""),
-      b= prompt("Насколько оценете его?",""),
-      c= prompt("Один из последних просмотренных фильмов?", ""),
-      d= prompt("Насколько оценете его?","");
+// const a= prompt("Один из последних просмотренных фильмов?", ""),
+//       b= prompt("Насколько оценете его?",""),
+//       c= prompt("Один из последних просмотренных фильмов?", ""),
+//       d= prompt("Насколько оценете его?","");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
 
+console.log( NaN || 2 || undefined );
+console.log( NaN && 2 && undefined );
+console.log( 1 && 2 && 3 );
+console.log( !1 && 2 || !3 );
+console.log( 25 || null && !3 );
+console.log( NaN || null || !3 || undefined || 5);
+console.log( NaN || null && !3 && undefined || 5);
+console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!');
+}else {console.log('<jkn')}git
 
 
 
