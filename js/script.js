2a4c05574e9b287a12776d04dd2f2635315256ce
Alina <example@gmail.com>
2022-07-31 15:25:44 +0300

    цикл

--- js/script.js
+++ js/script.js
@@ -65,14 +65,21 @@ console.log( 5 === 5 && 3 > 1 || 5);
 //    console.log('Done!')
 // }
 
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
  
-if (hamburger && cola || fries === 3 && nuggets) {
-   console.log('Done!');
-}else {console.log('<jkn')}git
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// }else {console.log('<jkn');}
+
+
+let num=50;
+for (let i=1; i <= 8; i++) {
+   console.log(num);
+   num++;
+}
 
 
 
