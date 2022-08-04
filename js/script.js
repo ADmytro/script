101a70de27edb4a04b80f6bafc77d65ef2f98e15
Alina <example@gmail.com>
2022-08-04 20:25:59 +0300

    задачи на цикл

--- js/script.js
+++ js/script.js
@@ -75,12 +75,136 @@ console.log( 5 === 5 && 3 > 1 || 5);
 // }else {console.log('<jkn');}
 
 
-let num=50;
-for (let i=1; i <= 8; i++) {
-   console.log(num);
-   num++;
+// let num=50;
+// for (let i=1; i <= 8; i++) {
+//    console.log(num);
+//    num++;
+// }
+
+// for (let i=0; i<3; i++){
+//    console.log(i);
+//    for (let j=0; j<4; j++){
+//       console.log(j);
+//    }
+// }
+
+// let result="";
+// const length=7;
+
+// for (let i=0; i<length; i++){
+//    for (let j=0; j<i; j++){
+//       result+="*";
+//    }
+//    result+="\n";
+// }
+// console.log(result);
+
+// first: for (let i=0; i<3; i++){
+//    console.log (`First level: ${i}`);
+//    for (let j=0; j<3; j++){
+//       console.log(`Second level: ${j}`);
+//       for(let k=0; k<3; k++){
+//          if (k===2) {break;}
+//          console.log(`Third level: ${k}`);
+//       }
+//    }
+// }
+
+// let num=5;
+// while (num<=10){
+//    console.log(num);
+//    num++;
+// }
+
+// for (let i=20; i>=10; i--){
+//    if (i===13){
+//       break;
+//    }
+//    console.log (i);
+// }
+
+// for (let i=2; i<=10; i = i+2){
+
+//    console.log(i);
+// }
+
+// for (let i = 2; i <= 16; i++) {
+//    if (i % 2 === 0) {
+//        continue;
+//    } else {
+//        console.log(i);
+//    }
+// }
+
+let i=1;
+while (i<15){
+   i = i+2;
+   console.log(i);
 }
 
+let i = 2;
+
+while (i <= 16) {
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    } else {
+        console.log(i);
+    }
+    i++;
+}
+
+const arrayOfNumbers = [];
+
+for (let i = 5; i < 11; i++) {
+    arrayOfNumbers[i - 5] = i;
+}
+
+console.log(arrayOfNumbers);
+
+
+
+
+// const arr=[1,2,3,4,5];
+// for (let i=0; i < arr.length; i++) {
+//    for (let j=5;)
+// }
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i];
+}
+
+console.log(result);
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for( let i=0; i<data.length; i++){
+    if (typeof(data[i])==="number" ){
+        data[i]=data[i]*2;
+    } else if (typeof(data[i])==="string"){
+        data[i]=data[i]+'-done';
+    }
+}
+console.log(data);
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+for( let i=4; i>=0; i--){
+    result[4-i]=data[i];
+}
+console.log(result);
+
+let result='';
+
+
+
+
+
+
+
+
+ 
+   
 
 
 
