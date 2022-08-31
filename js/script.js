61af342065027f197e3e40e1119b3a753bca735f
Alina <example@gmail.com>
2022-08-31 15:22:53 +0300

    практика4

--- js/script.js
+++ js/script.js
@@ -1,63 +1,94 @@
-// 'use strict';
-// // // let number=5;
-// // // const leftBorderWidth=5;
-
-// // // number=55;
-// // // console.log(number);
-
-// // // const obj={
-// // //     a:50
-// // // };   
-// // // console.log(obj);
-
-// // // alert('Hello');
-// // // const result=confirm ("Are you ready?");
-// // // console.log(result);
-
-// // // const answer=prompt("dff jglfj fjgk?", "");
-
-// // // console.log(typeof (answer));
-
-let numberOfFilms;
-function start (){
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
-    while ( numberOfFilms ==''|| numberOfFilms== null || isNaN( numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
-    }
-}
-start ();
+'use strict';
 
 const personalMovieDB={
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function(){
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели", "");
+        while ( personalMovieDB.count ==''|| personalMovieDB.count== null || isNaN( personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели", "");
+        }
+    },
+    rememberMyFilms: function(){
+        for (let i=0; i<2; i++){
+            const a= prompt("Один из последних просмотренных фильмов?", ""),
+                  b= prompt("Насколько оценете его?","");
+            
+            if (a != null && b!= null && a!=''&& b!='' && a.length < 50 && b.length < 50){
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+            personalMovieDB.movies[a] = b;
+        }
+    },
+    detectPersonalLevel: function(){ 
+        if(personalMovieDB.count <10) {
+            console.log('просмотрено мало фильмов');
+        } else if (personalMovieDB.count >=10 && personalMovieDB.count <30){
+            console.log('классический зритель');
+        }else if (personalMovieDB.count >=30){
+            console.log("Вы - киноман");
+        }
+    },
+    showMyDB: function(hidden){
+        if(!hidden){
+            console.log(personalMovieDB);
+        }
+    },
+//*     2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+//* переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+   
+    toggleVisibleMyDB: function(){
+        if(personalMovieDB.privat){
+            personalMovieDB.privat=false;
+        } else {
+            personalMovieDB.privat=true;
+        }
+    },
+// *    3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+// *Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+//* при помощи метода forEach вывести в консоль сообщения в таком виде:
+// *"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+    writeYourGenres: function(){
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+            if (genre === '' || genre == null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            } 
+            
+            // Альтернативный вариант из урока
+            
+            // let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
+
+            // if (genres === '' || genres == null) {
+            //     console.log('Вы ввели некорректные данные или не ввели их вовсе');
+            //     i--;
+            // } else {
+            //     personalMovieDB.genres = genres.split(', ');
+            //     personalMovieDB.genres.sort();
+            // } 
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    }
 };
-
-
-// console.log(personalMovieDB);
 
 // Практика часть2
 
 
-function rememberMyFilms(){
-    for (let i=0; i<2; i++){
-        const a= prompt("Один из последних просмотренных фильмов?", ""),
-              b= prompt("Насколько оценете его?","");
-        
-        if (a != null && b!= null && a!=''&& b!='' && a.length < 50 && b.length < 50){
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-        personalMovieDB.movies[a] = b;
-         
-    }
-}
-// rememberMyFilms();
+
 
 // let j=0;
 
@@ -80,283 +111,10 @@ function rememberMyFilms(){
 
 
 
-function detectPersonalLevel(){ 
-    if(personalMovieDB.count <10) {
-        console.log('просмотрено мало фильмов');
-    } else if (personalMovieDB.count >=10 && personalMovieDB.count <30){
-        console.log('классический зритель');
-    }else if (personalMovieDB.count >=30){
-        console.log("Вы - киноман");
-    }
-}
-// detectPersonalLevel();
-
-function showMyDB(hidden){
-    if(!hidden){
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres(){
-    for(let i=1; i<=3; i++){
-        personalMovieDB.genres[i-1]=prompt(`Ваш любимый жанр по номером ${i}`);
-    }
-}
-writeYourGenres();
-
-
-// // console.log( NaN || 2 || undefined );
-// // console.log( NaN && 2 && undefined );
-// // console.log( 1 && 2 && 3 );
-// // console.log( !1 && 2 || !3 );
-// // console.log( 25 || null && !3 );
-// // console.log( NaN || null || !3 || undefined || 5);
-// // console.log( NaN || null && !3 && undefined || 5);
-// // console.log( 5 === 5 && 3 > 1 || 5);
-
-// // // const hamburger = 3;
-// // // const fries = 3;
-// // // const cola = 0;
-// // // const nuggets = 2;
- 
-// // // if (hamburger === 3 && cola || fries === 3 && nuggets) {
-// // //    console.log('Done!')
-// // // }
-
-
-// // // let hamburger;
-// // // const fries = NaN;
-// // // const cola = 0;
-// // // const nuggets = 2;
- 
-// // // if (hamburger || cola || fries === 3 || nuggets) {
-// // //    console.log('Done!')
-// // // }
-
-// // // let hamburger;
-// // // const fries = NaN;
-// // // const cola = 0;
-// // // const nuggets = 2;
- 
-// // // if (hamburger && cola || fries === 3 && nuggets) {
-// // //    console.log('Done!');
-// // // }else {console.log('<jkn');}
-
-
-// // // let num=50;
-// // // for (let i=1; i <= 8; i++) {
-// // //    console.log(num);
-// // //    num++;
-// // // }
-
-// // // for (let i=0; i<3; i++){
-// // //    console.log(i);
-// // //    for (let j=0; j<4; j++){
-// // //       console.log(j);
-// // //    }
-// // // }
-
-// // // let result="";
-// // // const length=7;
-
-// // // for (let i=0; i<length; i++){
-// // //    for (let j=0; j<i; j++){
-// // //       result+="*";
-// // //    }
-// // //    result+="\n";
-// // // }
-// // // console.log(result);
-
-// // // first: for (let i=0; i<3; i++){
-// // //    console.log (`First level: ${i}`);
-// // //    for (let j=0; j<3; j++){
-// // //       console.log(`Second level: ${j}`);
-// // //       for(let k=0; k<3; k++){
-// // //          if (k===2) {break;}
-// // //          console.log(`Third level: ${k}`);
-// // //       }
-// // //    }
-// // // }
-
-// // // let num=5;
-// // // while (num<=10){
-// // //    console.log(num);
-// // //    num++;
-// // // }
-
-// // // for (let i=20; i>=10; i--){
-// // //    if (i===13){
-// // //       break;
-// // //    }
-// // //    console.log (i);
-// // // }
-
-// // // for (let i=2; i<=10; i = i+2){
-
-// // //    console.log(i);
-// // // }
-
-// // // for (let i = 2; i <= 16; i++) {
-// // //    if (i % 2 === 0) {
-// // //        continue;
-// // //    } else {
-// // //        console.log(i);
-// // //    }
-// // // }
-
-// // // let i=1;
-// // // while (i<15){
-// // //    i = i+2;
-// // //    console.log(i);
-// // // }
-
-// // // let i = 2;
-
-// // // while (i <= 16) {
-// // //     if (i % 2 === 0) {
-// // //         i++;
-// // //         continue;
-// // //     } else {
-// // //         console.log(i);
-// // //     }
-// // //     i++;
-// // // }
-
-// // // const arrayOfNumbers = [];
-
-// // // for (let i = 5; i < 11; i++) {
-// // //     arrayOfNumbers[i - 5] = i;
-// // // }
-
-// // // console.log(arrayOfNumbers);
 
 
 
 
-// // // // const arr=[1,2,3,4,5];
-// // // // for (let i=0; i < arr.length; i++) {
-// // // //    for (let j=5;)
-// // // // }
-// // // const arr = [3, 5, 8, 16, 20, 23, 50];
-// // // const result = [];
-// // // for (let i = 0; i < arr.length; i++) {
-// // //     result[i] = arr[i];
-// // // }
-
-// // // console.log(result);
-
-// // // const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-// // // for( let i=0; i<data.length; i++){
-// // //     if (typeof(data[i])==="number" ){
-// // //         data[i]=data[i]*2;
-// // //     } else if (typeof(data[i])==="string"){
-// // //         data[i]=data[i]+'-done';
-// // //     }
-// // // }
-// // // console.log(data);
-// // // const data = [5, 10, 'Shopping', 20, 'Homework'];
-// // // const result = [];
-// // // for( let i=4; i>=0; i--){
-// // //     result[4-i]=data[i];
-// // // }
-// // // console.log(result);
-
-// // // let result='';
-// // // const lenth=7;
-
-// // // for (let i=1; i<lenth; i++) {
-// // //     for(let j=0; j<i; j++){
-// // //         result+="*";
-// // //     }
-// // //     result+='\n';
-// // // }
-// // // console.log(result);
-
-// // let result='';
-// // let row=6;
-
-// // //     for(let j=0; j<12; j++){
-// // //         if (j%2===0){
-// // //             for (let i=0; i<5; i++){
-// // //                 result+=4-i;
-// // //             }
-// // //             continue;}
-// // //         else {for(let k=0; k<j; k++){
-// // //             result+="*";}
-// // //         }                            
-// // //        result+='\n';
-    
-// // // }
-// //     for (let i=1; i<row; i++) {
-// //         result+=" ";
-// //         for(let j=0; j<i*2; j++){
-// //             if (j%2===0){
-// //                 continue;}
-// //             else {for(let k=0; k<j; k++){
-// //                 result+="*";}
-// //         }
-// //         result+='\n';}
-// //     }
-  
-
-
-
-// // console.log(result);
-
-// // // const lines = 5;
-// // // let result = '';
-
-// // // for (let i = 0; i <= lines; i++) {
-// // //     for (let j = 0; j < lines - i; j++) {
-// // //         result += " ";
-// // //     }
-// // //     for (let j = 0; j < 2 * i + 1; j++) {
-// // //         result += "*";
-// // //     }
-// // //     result += "\n";
-// // // }
-
-// // // console.log(result)
-
-
-// function sayHello(name){
-//     return `Привет, ${name}!`;
-// }
-// sayHello ("Anton");
-
-// function sayHello(name) {
-//    console.log( `Привет, ${name}!`);
-    
-// }
-
-// sayHello('Alex');
-
-
-
-
-// function returnNeighboringNumbers(num) {
-//    return console.log([num-1, num, num+1]);
-// }
-// returnNeighboringNumbers (7);
-
-// let result='';
-// function getMathResult(a, m){
-//     if(m<=0 || typeof(m) !=='number'){
-//         return a;
-//      }else {
-//         for( let i=1; i<=m; i++ ){ 
-//             if ( i===m ){
-//             result +=a+(i-1)*a; }
-//             else{
-//                 result +=a+(i-1)*a+'---';
-//             }  
-//             }
-//     }
-//     return result;  
-// } 
-// getMathResult (5, 4);
 
 
 
